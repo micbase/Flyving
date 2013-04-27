99d50111cb24d2b48ad7e3ea0cfec79d2f7615b2
Michael Chang <michael@micbase.com>
2013-04-27 13:28:46 -0500

    add some fish, move randomly

---
+++ Creature.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace AssemblyCSharp
+{
+	public class Creature
+	{
+		
+		int iType = 1;
+		int iSpeed = 1;
+		
+		public Creature()
+		{
+		}
+		
+		public void Add()
+		{
+		}
+		
+		public void Remove()
+		{
+		}
+		
+		
+	}
+}
+
